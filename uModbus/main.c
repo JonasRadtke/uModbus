1b8d4e4a55352bf4fdf17a9c4fb6fac4c62aea81
JRadtke <jonas@radtke.dk>
2024-01-15 23:34:55 +0100

    update readme

--- uModbus/main.c
+++ uModbus/main.c
@@ -7,24 +7,24 @@
 #include <asf.h>
 #include "main.h"
 
-	volatile uint32_t ticks = 0; // Time since Start (ms)
-	mbDataMapping modbusData;
-	mbComm modbusComm;
+volatile uint32_t ticks = 0; // global systick timer (overflows after 49 Days at 1ms)
+mbDataMapping modbusData;
+mbComm modbusComm;
 
 int main (void)
 {
 	system_init();
-	SysTick_Config(48000000 / 1000); //Systemtimer config
+	SysTick_Config(48000000 / 1000); //Systemtimer config 1ms
 
 	uartInit();
 
 	// Test Data
-	modbusData.inputRegisters[0] = 125;
-	modbusData.inputRegisters[1] = 50000;
-	modbusData.holdingRegisters[0] = 500;
-	modbusData.holdingRegisters[1] = 60000;
-	modbusData.coils[0] = 0xDC;
-	modbusData.coils[1] = 0x66;
+	//	modbusData.inputRegisters[0] = 125;
+	//	modbusData.inputRegisters[1] = 50000;
+	//	modbusData.holdingRegisters[0] = 500;
+	//	modbusData.holdingRegisters[1] = 60000;
+	//	modbusData.coils[0] = 0xDC;
+	//	modbusData.coils[1] = 0x66;
 
 	while (1)
 	{
@@ -32,6 +32,7 @@ int main (void)
 	}
 }
 
+// Cyclic Systemtimer 1ms call
 void SysTick_Handler(){
 	ticks++;
 	MBTimer();
